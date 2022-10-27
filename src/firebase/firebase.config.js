d8809e898de8411ddb90462f628daf6d9b1c6717
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-10-27 11:15:46 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBX1yxVKG4ysKwVy5UXxGNz8_r1mnRPvLQ",
+  authDomain: "lmsone-client.firebaseapp.com",
+  projectId: "lmsone-client",
+  storageBucket: "lmsone-client.appspot.com",
+  messagingSenderId: "1050907665302",
+  appId: "1:1050907665302:web:c30047fb08c71ca96ab3b7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/layout/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../pages/shared/Header/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <h1>this is main</h1>
+    </div>
+  );
+};
+
+export default Main;
